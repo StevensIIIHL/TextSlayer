c867a5b8817b9e2537f9ff76ee8fd5606ccd36f2
StevensIIIHL <106164186+StevensIIIHL@users.noreply.github.com>
2025-05-15 22:11:44 -0400

    Create MapGrid.java
    Added new file and code within to project

---
+++ src/textslayer/MapGrid.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package textslayer;
+
+/**
+ *
+ * @author sinn3
+ */
+public class MapGrid {
+    private static final int SIZE = 8;
+    private final Position[][] grid;
+
+    public MapGrid() {
+        grid = new Position[SIZE][SIZE];
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                grid[x][y] = new Position(x, y);
+            }
+        }
+    }
+
+    public boolean isValidMove(Team team, int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        return grid[x][y].getOccupants().getOrDefault(team, 0) < 2;
+    }
+
+    public void display() {
+        System.out.println("\nCurrent Battlefield:");
+        for (int y = 0; y < SIZE; y++) {
+            for (int x = 0; x < SIZE; x++) {
+                Position pos = grid[x][y];
+                String red = pos.getOccupants().getOrDefault(Team.RED, 0) > 0 ? 
+                    "R" + pos.getOccupants().get(Team.RED) : "";
+                String blue = pos.getOccupants().getOrDefault(Team.BLUE, 0) > 0 ? 
+                    "B" + pos.getOccupants().get(Team.BLUE) : "";
+                System.out.printf("[%-5s]", red + " " + blue);
+            }
+            System.out.println();
+        }
+    }
+
+    public Position getPosition(int x, int y) {
+        return grid[x][y];
+    }
+}
